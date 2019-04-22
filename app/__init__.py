5134048f1c4794688c6be9d6d8c76c431f205e00
Mitchell Moore <mmoo97@uab.edu>
2019-04-22 10:16:38 -0500

    Updated return_url with placeholder value for stability.

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,10 @@
 # app/__init__.py
 
 # local imports
+from __future__ import print_function
+
 import os
+import sys
 
 # third-party imports
 from flask import Flask, redirect, url_for, request
@@ -14,39 +17,38 @@ def create_app(config_name):
     app = Flask(__name__)
     Bootstrap(app)
     # TODO:// test return URL. *Possibly make a success page before returning?*
-    return_url = request.referrer # 'https://www.google.com/'
+    global return_url
+    return_url = ''
 
     @app.route('/success/<name>/<username>')
     def success(username, name):
 
-        # TODO: Deliver arguments to script.
-
-        tempString = 'sudo user_create ' + username + " " + name
-        os.system("ssh ohpc " + tempString)
-
-        # TODO: Make the success route back to cheaha.
-
-        # return "Username: %s | Name: %s" % (username, name)
+        global return_url
+        print(username, name,return_url, file=sys.stdout)
+        # Deliver arguments to script.
+        # tempString = 'sudo user_create ' + username + " " + name
+        # os.system("ssh ohpc " + tempString)
 
         return redirect(return_url, 302)
 
     @app.route('/', methods=['GET'])
     def index():
 
-        # TODO:// test remote user variable
+        # TODO: Make url origin url.
+        global return_url
+        return_url = "http://example.com"
 
         user = request.remote_user
 
-        # user = 'tom'
-
-        return render_template("auth/SignUp.html", user=user)
+        return render_template("auth/SignUp.html", user=user, url=return_url)
 
     @app.route('/', methods=['GET', 'POST'])
     def SignUp():
 
         name = request.form['name']
-        user = request.environ('REMOTE_USER')
-        # user = 'tom'
+
+        # TODO: Test to make sure remote_user is captured
+        user = request.remote_user
 
         if request.method == 'GET':
 
@@ -54,7 +56,7 @@ def create_app(config_name):
 
         if request.method == 'POST' and name != "":
 
-            return redirect(url_for('success', username=user, name=name))
+            return redirect(url_for('success', username=str(user), name=name))
 
         else:
             return render_template("auth/SignUp.html", user=user)
