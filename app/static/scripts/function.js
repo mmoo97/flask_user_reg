0fb437e01b9133af39fe51326b800642ca452db7
ishan747 <ishan747@uab.edu>
2019-10-22 17:41:16 +0000

    removed function from signup.html

---
+++ app/static/scripts/function.js
@@ -0,0 +1,21 @@
+<script>
+        function displayloading() {
+            document.getElementById("notification_window").innerHTML = "" +
+                "<div class=\"joyride-tip-guide\" data-index=\"0\" style=\"visibility: visible; display: block; top: 77.5px; left: 570px;\"><span class=\"joyride-nub\" style=\"display: none;\"></span><div class=\"joyride-content-wrapper\" role=\"dialog\"><ol>\n" +
+                "        <h4>\n" +
+                "\n" +
+                "          Your account is pending creation...\n" +
+                "\n" +
+                "        </h4>\n" +
+                "                <br><p>\n" +
+                "                    This can take between 5-10 min.\n" +
+                "                </p>\n" +
+                "      </ol>\n" +
+                "                <img src=\"/register/static/img/loading.gif\" style=\"width: 35px; height: auto\"> Pending...\n" +
+                "            </div></div>\n" +
+                "\n" +
+                "    <div class=\"joyride-modal-bg\" style=\"display: block;\"></div>"
+
+
+        }
+    </script>
