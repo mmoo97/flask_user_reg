c43ebdc0d3292300981b12e3d150f72917fb3e53
Mitchell Moore <mmoo97@uab.edu>
2019-11-22 11:25:25 -0600

    get into runnable state

--- run.py
+++ run.py
@@ -3,11 +3,14 @@
 import os
 
 from app import create_app
+from flask_socketio import SocketIO
 
 config_name = os.getenv('FLASK_CONFIG')
 app = create_app(config_name)
 app.secret_key = 'randomString'
+socketio = SocketIO(app)
 
 
 if __name__ == '__main__':
-    app.run()
+   # app.run()
+    socketio.run(app)
